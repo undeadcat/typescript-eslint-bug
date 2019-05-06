375ee88d2f927683bad75f3acce6de231050cc9f
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-05-06 22:10:24 +0300

    initial

---
+++ ts.ts
@@ -0,0 +1,17 @@
+
+declare var React;
+declare var PropTypes;
+const SHOULD_BE_UNUSED = 1;
+
+const INPUT_PASS_PROPS = {
+    autoFocus: true,
+    disabled: true,
+    warning: true,
+    error: true,
+    size: true,
+    onKeyDown: true,
+};
+
+console.log(PropTypes.number);
+console.log(React.Component);
+console.log(INPUT_PASS_PROPS);
